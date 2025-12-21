a6bbd6abc98d1c12f8af1d07888a85e353ec8e0f
Saliha Peker <salihapeker3@gmail.com>
2025-12-21 13:13:45 +0300

    Open-closed, generic class ornegi

---
+++ Uyg2Generiic/Uyg2Generiic/Islemler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Uyg2Generiic
+{
+    //internal class Islemler
+    internal class Islemler<T> //open closed prensibi generic class 
+    {
+        //int[] dizi = new int[10];
+        T[] dizi = new T[10];
+        //string[] dizi2 = new string[10];
+        int ESay = 0; //nonstatic tanımlandı. static olsaydı bütün nesneler için aynı ESay değeri olurdu. her yeni nesne türettiğimizde yeniden sıfırlanıo nonstatic olduğu için 
+
+       // public void elemanEkle(int eleman)
+        public void elemanEkle(T eleman)
+        {
+            dizi[ESay] = eleman;
+            ESay++;
+            Console.WriteLine(ESay);
+        }
+
+        //public int elemanDonder(int indis)
+        public T elemanDonder(int indis)
+        {
+            return dizi[indis];
+
+        }
+    }
+}
---
+++ Uyg2Generiic/Uyg2Generiic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg2Generiic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Islemler d = new Islemler();
+           // d.elemanEkle(3);
+           // int c = d.elemanDonder(0);
+
+            Islemler <int> d1 = new Islemler<int>();
+            d1.elemanEkle(5);
+            d1.elemanEkle(3);
+
+            Islemler<String> d2 = new Islemler<String>();
+            d2.elemanEkle("ali");
+            d2.elemanEkle("batu");
+            d2.elemanEkle("akay");
+
+            Islemler<Kitap> d3 = new Islemler<Kitap>();
+            d3.elemanEkle(new Kitap());
+            Kitap k1 = new Kitap();
+            d3.elemanEkle(k1);
+
+
+        }
+    }
+}
