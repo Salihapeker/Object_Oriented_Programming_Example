3ad46a4d502927ba8c7e35d70bb418b1b97b6f26
Saliha Peker <salihapeker3@gmail.com>
2025-12-24 15:44:27 +0300

    IComparable ornegi

---
+++ Uyg5ArraySrt/Uyg5ArraySrt/Personel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg5ArraySrt
+{
+    internal class Personel: IComparable
+    {
+        public int sicilNo;
+        public string adi;
+        public string soyadi;
+
+        public Personel(int sn, string a, string s)
+        {
+            sicilNo = sn;
+            adi = a;
+            soyadi = s;
+        }
+
+        public void yaz()
+        {
+            Console.WriteLine("{0}:{1} {2}", sicilNo, adi, soyadi);
+        }
+        public int CompareTo(object o)
+        {
+            Personel p = (Personel)o;
+            return adi.CompareTo(p.adi); // hangi fieldla karşılaştırma yapılacak onu tanımlıoruz.
+          //  return sicilNo.CompareTo(p.sicilNo);
+        }
+    }
+}
---
+++ Uyg5ArraySrt/Uyg5ArraySrt/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg5ArraySrt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] dizi = new int[] { -2, 3, 0, 5, 20, 1, -10 };
+            /* string[] dizi = new string[] { "ayse","ali", "ahmet", "mehmet", "deniz", "lale" };
+
+              Console.WriteLine("sıralama öncesi");
+              for(int i = 0; i < dizi.Length; i++)
+                  Console.WriteLine(dizi[i]);
+
+              Array.Sort(dizi);
+              Console.WriteLine("sıralama sonrasi");
+              for (int i = 0; i < dizi.Length; i++)
+                  Console.WriteLine(dizi[i]);*/
+
+            Personel[] dizi = new Personel[5];
+            dizi[0] = new Personel(105, "ali", "demir");
+            dizi[1] = new Personel(100, "ahmet", "gunes");
+            dizi[2] = new Personel(102, "ayse", "lale");
+            dizi[3] = new Personel(101, "mehmet", "celik");
+            dizi[4] = new Personel(110, "fatma", "deniz");
+            Console.WriteLine("siralama oncesi");
+            for (int i = 0; i < 5; i++) 
+            dizi[i].yaz();
+            Array.Sort(dizi);
+            Console.WriteLine("siralama sonrasi");
+            for (int i = 0; i < 5; i++)
+                dizi[i].yaz();
+        }
+    }
+}
