e6234e2ae39085e180b6d24cff85a7be28d6a3ca
Saliha Peker <salihapeker3@gmail.com>
2025-12-24 15:41:41 +0300

    Sealed method

---
+++ Uyg2SealedMethod/Uyg2SealedMethod/A.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg2SealedMethod
+{
+    internal class A
+    {
+        public virtual void method()
+        {
+            Console.WriteLine("Anin metodu");
+        }
+    }
+
+    class B : A
+    {
+        public override void method()
+        {
+            Console.WriteLine("Bnin metodu");
+        }
+
+      
+    }
+    class C : B
+    {
+        public sealed override void method()
+        {
+            Console.WriteLine("C nin metodu");
+        }
+    }
+    class D : C
+    {
+        /* public override void method()
+         {
+             Console.WriteLine("Dnin metodu");
+         }*/
+        public new void method()
+        {
+            Console.WriteLine("Dnin metodu");
+        }
+    }
+}
---
+++ Uyg2SealedMethod/Uyg2SealedMethod/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg2SealedMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+           // a.method();  //A
+            
+            B b = new B();
+          //  b.method(); //B
+
+
+            D d = new D();
+          //  d.method(); //D // D yi çağırmak için kabuk üzerinden çağırmamız gerekir
+
+            A[] dizi =new A[5];
+          /*  dizi[0] = new A(); //A
+            dizi[1] = new B(); //B
+            dizi[2] = new C(); //C
+            dizi[3] = new D(); //C
+            dizi[4] = new A(); //A */
+
+            dizi[0] = new B(); //B
+            dizi[1] = new B(); //B
+            dizi[2] = new C(); //C
+            dizi[3] = new D(); //C
+            dizi[4] = new C(); //C
+            for (int i = 0; i < 5; i++)
+            {
+                dizi[i].method();
+            }
+
+        }
+    }
+}
