d98d09f1c2b7bfe25ae4a8ea6a93c0306d55f6e7
Saliha Peker <salihapeker3@gmail.com>
2025-12-31 13:35:03 +0300

    Odeme ornegi

---
+++ OCPExmp1/OCPExmp1/Odeme.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCPExmp1
+{
+    class Odeme
+    {
+
+        public void Ode(OdemeYontemi o, int miktar)
+        {
+            o.Ode(miktar);
+        }
+    }
+
+    class CreditCard:OdemeYontemi
+    {
+
+        public void Ode(int miktar)
+        {
+            Console.WriteLine("Kredi kartı ile ödendi");
+        }
+    }
+
+    class Cash:OdemeYontemi
+    {
+        public void Ode(int miktar)
+        {
+            Console.WriteLine("Nakit ödendi");
+        }
+    }
+
+    class Havale:OdemeYontemi
+    {
+        public void Ode(int miktar)
+        {
+            Console.WriteLine("Havale ile ödendi");
+        }
+    }
+
+    interface OdemeYontemi
+    {
+        void Ode(int a);
+    }
+
+    class EFT : OdemeYontemi
+    {
+        public void Ode(int miktar)
+        {
+            Console.WriteLine("EFT ile ödendi");
+
+        }
+    }
+}
---
+++ OCPExmp1/OCPExmp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCPExmp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCard a = new CreditCard();
+            Odeme odemeNesnesi = new Odeme();
+            Cash c = new Cash();
+            Havale h = new Havale();
+            EFT e = new EFT();
+            odemeNesnesi.Ode(e,100);
+
+        }
+    }
+}
