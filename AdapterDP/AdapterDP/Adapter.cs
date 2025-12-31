de475cc524b94604a3ff79d49feb0ad49a05efcb
Saliha Peker <salihapeker3@gmail.com>
2025-12-31 13:21:26 +0300

    Adaptor ornegi

---
+++ AdapterDP/AdapterDP/Adapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDP
+{
+    class Adapter:Onceki
+    {
+        public override void sarjEt(int a)
+        {
+            base.sarjEt(a);
+            AdapteEdilen nesne = new AdapteEdilen();
+            nesne.Calistir(3,5);
+        }
+    }
+}
